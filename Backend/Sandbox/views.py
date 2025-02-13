8e86d4b7a1913743319e0bcc689d2d1555275c51
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2025-02-13 14:32:19 +0530

    chore: code cleanup - removed unnecessary files & comments

--- Backend/Sandbox/views.py
+++ Backend/Sandbox/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .code_executor.docker_manager import DockerManager
 
-
-# Create your views here.
 
 @api_view(['POST'])
 def execute_code(request):
@@ -13,9 +10,7 @@ def execute_code(request):
     
     docker_manager = DockerManager()
     output = docker_manager.execute(code, language)
-    
-    # output = f"Received {language} code: {code}"
-    
+
     return Response({
         'output': output,
         'language': language
